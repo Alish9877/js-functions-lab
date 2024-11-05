f7c68dbb03bb776f2aa3404d1c2b72767a2c3b8e
alish9877 <alshaikhali302@gmail.com>
2024-11-05 12:23:44 +0300

    ex3 done

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const IsAdult = (age) => {
-  if (age >= 18) {
-    return 'Adult'
-  } else {
-    return 'Major'
-  }
+// const IsAdult = (age) => {
+//   if (age >= 18) {
+//     return 'Adult'
+//   } else {
+//     return 'Major'
+//   }
+// }
+
+// console.log('Exercise 2 Result:', IsAdult(1))
+
+// //////
+
+const isCharAVowel = (char) => {
+  if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u')
+    return true
+  else return false
 }
 
-console.log('Exercise 2 Result:', IsAdult(1))
+console.log('Exercise 3 Result:', isCharAVowel('b'))
